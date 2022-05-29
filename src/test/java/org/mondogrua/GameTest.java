17e099750cd8e5739e2f30620e35a3b495418d97
Marco Testa <marco.testa@intre.it>
2022-05-29 10:42:03 +0200

    refactor rename class

--- src/test/java/org/mondogrua/GameTest.java
+++ src/test/java/org/mondogrua/GameTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-public class FrameTest {
+public class GameTest {
 
     Game game;
     private void addRolls(int[] rolls) {
