691b2304858dc55ce665ee25b0356564c01dc771
Marco Testa <marco.testa@intre.it>
2022-07-23 21:01:53 +0200

    frames as a chain instead of an array

--- src/main/java/org/mondogrua/Game.java
+++ src/main/java/org/mondogrua/Game.java
@@ -2,31 +2,28 @@ package org.mondogrua;
 
 public class Game {
 
-    private final Frame[] frames = new Frame[10];
+    private final Frame framesHead = new Frame(1, null);
 
     public Game() {
-        Frame previousFrame = null;
-        for (int frameIdx = 0; frameIdx <= 9; frameIdx++) {
-            Frame frame = new Frame(frameIdx+1, previousFrame);
-            if (previousFrame != null) {
-                previousFrame.setNextFrame(frame);
-            }
+        Frame previousFrame = framesHead;
+        for (int frameIdx = 2; frameIdx <= 10; frameIdx++) {
+            Frame frame = new Frame(frameIdx, previousFrame);
+            previousFrame.setNextFrame(frame);
             previousFrame = frame;
-            frames[frameIdx] = frame;
         }
     }
 
     public void add(Roll roll) {
-        this.frames[0].add(roll);
+        this.framesHead.add(roll);
     }
 
     public Integer getScore() {
-        return frames[0].getPartialScoreOr(0);
+        return framesHead.getPartialScoreOr(0);
     }
 
     public String getReport() {
         ReportAccumulator reportAccumulator = new ReportAccumulator();
-        frames[0].addReportTo(reportAccumulator);
+        framesHead.addReportTo(reportAccumulator);
         return reportAccumulator.value();
     }
 }
